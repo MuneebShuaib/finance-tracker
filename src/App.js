8f36ce0a15a48ba3d560d6a0576e50c133384909
Trident White <muneebshuaib100@gmail.com>
2022-12-19 22:00:21 -0500

    the start of my finance-tracker website, all front end stuff in this commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <div className ='left-side-bar'>
+          Fuck off ya bluddy clart
+        </div>
+        <div className = 'main-content'>
+          <section>
+          </section>
+
+          <section>
+          </section>
+        </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Transactions.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Transactions = ()=>{
+    return(
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    )
+
+}
+
+
+export default Transactions
