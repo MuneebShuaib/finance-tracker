17cef2c7ce5a2d5c60036c9b14a669d0dbad01c6
Trident White <muneebshuaib100@gmail.com>
2023-01-25 15:02:00 -0500

    error handling

--- src/backend/server.js
+++ src/backend/server.js
@@ -19,5 +19,6 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/api/transactions', routes)
 app.use(errorHandler)
+//run express server on PORT, and console log 'server connected on port 5000'
 app.listen(PORT, ()=> console.log(`server connected on port ${PORT}`))
 
