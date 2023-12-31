7aa0cd40c2eadfe9fc0d12008e63aa4bfb88f76c
Trident White <muneebshuaib100@gmail.com>
2023-12-31 00:53:12 -0500

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const routes = require('./routes/transactionRoutes')
 const {errorHandler} = require('./middleware/errorMidleware')
 require('dotenv').config()
-
+const path = require('path')
 
 
 connectDB()
@@ -12,7 +12,6 @@ const app = express()
 
 
 PORT = process.env.PORT 
-PORT2 = process.env.PORT2
 //in order to use body data, we just have to add a couple lines of middleware
 //middleware:
 app.use(express.json())
@@ -21,9 +20,19 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/transactions', routes)
 app.use('/api/users', require("./routes/userRoutes"))
 
+//Serve frontend
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => 
+    res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}else{
+    app.get('/', (req, res) => res.send("not set on production"))
+}
 app.use(errorHandler)
 //run express server on PORT, and console log 'server connected on port'
 //app.listen(PORT, ()=> console.log(`server connected on port ${PORT}`))
 
-app.listen(PORT || PORT2, ()=> console.log(`server connected on port bhenchod`))
+app.listen(process.env.PORT, ()=> console.log(`server connected on port bhenchod`))
 
